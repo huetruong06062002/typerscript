c8ac75a1b397308186eb72acfac88530cb6dc416
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-01-13 18:19:25 +0700

    Type anotation TS

---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,6 @@
+let countTS:number = 1;
+// countTS = "an";   //error
+
+let namesTS: string[ ] = ['a', 'b', 'c'] // array chỉ bao gồm string
+//namesTS.push(1) //error
+console.log(namesTS);
---
+++ projects/lesson6/lesson6JS.js
@@ -0,0 +1,6 @@
+let count = 1;
+count = "an";
+
+let names = ['a', 'b', 'c'] // array chỉ bao gồm string
+names.push(1) //error
+console.log(names);
