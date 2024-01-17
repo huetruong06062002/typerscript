bfc54fabc2169419b39b0c850e3670240fbc25da
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-01-17 11:54:10 +0700

    Tupple type in TS

---
+++ projects/lesson13/lesson13.ts
@@ -0,0 +1,15 @@
+//tuples : dataType, value, size
+
+let skills:(number | string)[] = [25, "languages", 96];
+
+let skills3 : [number, string] = [96, "language"];
+
+let bgColor, headerColor: [number, number, number, number?];
+bgColor = [0,0,255,0];
+headerColor = [0,0,255];
+
+
+
+console.log(skills3);
+
+console.log(bgColor, headerColor);
